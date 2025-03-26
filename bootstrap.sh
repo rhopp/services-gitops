e42dd2798273460540c0a3dabe070f2d001b4733
Radim Hopp <rhopp@redhat.com>
2025-03-26 14:37:22 +0100

    .

---
+++ bootstrap.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+create_subscription() {
+	echo "Installing the OpenShift GitOps operator subscription:"
+	kubectl apply -k "$ROOT/components/openshift-gitops"
+	echo -n "Waiting for default project (and namespace) to exist: "
+	while ! kubectl get appproject/default -n openshift-gitops &>/dev/null; do
+		echo -n .
+		sleep 1
+	done
+	echo "OK"
+}
+
+wait_for_route() {
+	echo -n "Waiting for OpenShift GitOps Route: "
+	while ! kubectl get route/openshift-gitops-server -n openshift-gitops &>/dev/null; do
+		echo -n .
+		sleep 1
+	done
+	echo "OK"
+}
+
+grant_admin_role_to_all_authenticated_users() {
+	echo Allow any authenticated users to be admin on the Argo CD instance
+	# - Once we have a proper access policy in place, this should be updated to be consistent with that policy.
+	kubectl patch argocd/openshift-gitops -n openshift-gitops -p '{"spec":{"rbac":{"policy":"g, system:authenticated, role:admin"}}}' --type=merge
+}
+
+set_trackingmethod_annotation() {
+	echo "Setting ArgoCD tracking method to annotation"
+	kubectl patch argocd/openshift-gitops -n openshift-gitops -p '
+spec:
+  resourceTrackingMethod: annotation
+' --type=merge
+}
+
+
+create_namespace_and_AppProject() {
+	echo "Creating namespace gitops-resources"
+	oc new-project gitops-resources
+
+	echo "Creating new AppProject"
+	kubectl apply -f - <<-EOF
+	apiVersion: argoproj.io/v1alpha1
+	kind: AppProject
+	metadata:
+		name: gitops-resources
+		namespace: openshift-gitops
+	spec:
+		destinations:
+			- name: '*'
+				namespace: '*'
+				server: '*'
+		sourceNamespaces:
+			- gitops-resources
+		sourceRepos:
+			- '*'
+	EOF
+}
+
